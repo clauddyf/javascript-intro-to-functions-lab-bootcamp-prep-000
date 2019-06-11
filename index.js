65002ba3e4f8ab9d8f14458fcd70d33b082a732a
Claudius E Solomon <claudius.solomon@gmail.com>
2019-06-11 07:57:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!" 
   } elseif (shout(string)) {
     return "YES INDEED!"
-  } else { (console.log("I love you, too."))
+  } elseif { (console.log("I love you, too."))
   
     
   
