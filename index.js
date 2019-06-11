bf9d1f29d935f688a19417b3af9c420042e9a7ab
Claudius E Solomon <claudius.solomon@gmail.com>
2019-06-11 07:52:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (whisper(string)) {
-    return "I can\'t hear you!" }
-  elseif
+    return "I can\'t hear you!" 
+  } elseif
     
   
 }
